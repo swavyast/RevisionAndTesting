1d4180fea83a959ad2762472203eb66f87ed86ba
Himanshu Rai <swavyast@gmail.com>
2023-10-10 11:37:22 +0530

    testing Min and Max values of primitive types

---
+++ src/com/ml/revision/dataTypes/MaxValues.java
@@ -0,0 +1,24 @@
+package com.ml.revision.dataTypes;
+
+public class MaxValues {
+	
+	public static void main(String[] args) {
+		System.out.println("Min Value of Byte : "+Byte.MIN_VALUE);
+		System.out.println("Max Value of Byte : "+Byte.MAX_VALUE);
+		System.out.println("Min Value of Short : "+Short.MIN_VALUE);
+		System.out.println("Max Value of Short : "+Short.MAX_VALUE);
+		System.out.println("Min Value of Integer : "+Integer.MIN_VALUE);
+		System.out.println("Max Value of Integer : "+Integer.MAX_VALUE);
+		System.out.println("Min Value of Long : "+Long.MIN_VALUE);
+		System.out.println("Max Value of Long : "+Long.MAX_VALUE);
+		System.out.println("Min Value of Float : "+Float.MIN_VALUE);
+		System.out.println("Max Value of Float : "+Float.MAX_VALUE);
+		System.out.println("Min Value of Double : "+Double.MIN_VALUE);
+		System.out.println("Max Value of Double : "+Double.MAX_VALUE);
+		System.out.println("Min Value of Character : "+Character.MIN_VALUE);
+		System.out.println("Max Value of Character : "+Character.MAX_VALUE);
+		System.out.println("Boolean False : "+Boolean.FALSE);
+		System.out.println("Boolean True : "+Boolean.TRUE);
+	}
+
+}
