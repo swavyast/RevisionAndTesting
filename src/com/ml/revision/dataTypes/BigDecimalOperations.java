cdccde89176e96fca97e15fd714dd9bc53018c40
Himanshu Rai <swavyast@gmail.com>
2023-11-08 19:06:40 +0530

    bin/com/ml/revision/dataTypes/BigDecimalOperations.class
    src/com/ml/revision/dataTypes/BigDecimalOperations.java

--- src/com/ml/revision/dataTypes/BigDecimalOperations.java
+++ src/com/ml/revision/dataTypes/BigDecimalOperations.java
@@ -1,27 +1,38 @@
 package com.ml.revision.dataTypes;
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.math.MathContext;
 
 public class BigDecimalOperations {
 
+    
     public static void main(String[] args) {
+	
 	System.out.println(BigDecimal.ZERO);
 	System.out.println(BigDecimal.ONE);
 	System.out.println(BigDecimal.TWO);
 	System.out.println(BigDecimal.TEN);
 	System.out.println("\n*************************************************************");
-	//BigDecimal Values
+		
+		//BigDecimal Values
 	BigDecimal firstValue=new BigDecimal("12256984789964558.556988487555896");
 	BigDecimal secondValue=new BigDecimal("122569847899645567.5569882387555896");
 	
-	//BigDecimalOperations
+		//BigDecimalOperations
 	BigDecimal sum=firstValue.add(secondValue);
 	BigDecimal sub=secondValue.subtract(firstValue);
 	BigDecimal pro=firstValue.multiply(secondValue);
+        	/**
+        	 * throws
+        	 * Exception in thread "main" java.lang.ArithmeticException: Non-terminating decimal expansion; 
+        	 * no exact representable decimal result.
+        	 * 
+        	 * Reason : In the case of divide, the exact quotient could have an infinitely long decimal expansion; 
+        	 * for example, 1 divided by 3.
+        	 * */
+		//BigDecimal quo=secondValue.divide(firstValue);
 	BigDecimal quo=secondValue.divide(firstValue, MathContext.DECIMAL128);
 	BigDecimal rem=firstValue.remainder(secondValue);
-	//PrintResults
+		//PrintResults
 	System.out.println("Sum of firstValue and secondValue is : \n\n"+sum.toString());
 	System.out.println("\nDifference of firstValue and secondValue is : \n\n"+sub.toString());
 	System.out.println("\nProduct of firstValue and secondValue is : \n\n"+pro.toString());
@@ -52,19 +63,19 @@ public class BigDecimalOperations {
         	 * */
 	System.out.println("Scale of firstValue is : "+firstValue.scale());
 	
-	//Returns a string representation of this BigDecimal without an exponent field.
+		//Returns a string representation of this BigDecimal without an exponent field.
 	System.out.println("PlainStringValue of firstValue is : "+firstValue.toPlainString());
 	
-	//Returns a BigDecimal whose value is the absolute value of this BigDecimal, and whose scale is this.scale().
+		//Returns a BigDecimal whose value is the absolute value of this BigDecimal, and whose scale is this.scale().
 	System.out.println("AbsoluteValue of firstValue is : "+firstValue.abs());
 	
-	//Returns the numeric value represented by this object after conversion to type byte.
+		//Returns the numeric value represented by this object after conversion to type byte.
 	System.out.println("ByteValue of firstValue is : "+firstValue.byteValue());
 	
 	System.out.println("ByteValue of secondValue is : "+secondValue.byteValue());
 	System.out.println("\n************************************");
 	
-	//Using Number Type reference for storing BigDecimal Type values
+		//Using Number Type reference for storing BigDecimal Type values
 	Number n1=new BigDecimal("23.69");
 	System.out.println(n1.doubleValue());
 	System.out.println(n1.intValue());
