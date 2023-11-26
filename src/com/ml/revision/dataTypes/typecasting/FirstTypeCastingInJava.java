9c08b7b8586efb07a1396642619823302170157b
Himanshu Rai <swavyast@gmail.com>
2023-11-26 12:08:10 +0530

    I0000000000000000000000000000000000000010

---
+++ src/com/ml/revision/dataTypes/typecasting/FirstTypeCastingInJava.java
@@ -0,0 +1,24 @@
+package com.ml.revision.dataTypes.typecasting;
+
+public class FirstTypeCastingInJava {
+
+    public static void main(String[] args) {
+	/*
+	 * smaller data types can be directly type-casted into larger data types without doing anything in particular.
+	 * this kind of type-casting is known as implicit type-casting.
+	 * */
+	byte x=97;
+	short y=x;
+	int a=y;
+	long b=a;
+	float c=b;
+	double d=c;
+	System.out.println("\n\nbyte representation : "+x);
+	System.out.println("\n\nshort representation : "+y);
+	System.out.println("\n\nint representation : "+a);
+	System.out.println("\n\nlong representation : "+b);
+	System.out.println("\n\nfloat representation : "+c);
+	System.out.println("\n\ndouble representation : "+d);
+    }
+
+}
