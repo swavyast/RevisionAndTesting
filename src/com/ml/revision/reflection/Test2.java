6a0bf80fad4ce6b8fe869ab8062c0e3bc1c3b095
Himanshu Rai <swavyast@gmail.com>
2024-04-07 03:09:19 +0530

    I0000000000000000000000000000000000000033
    Signed-off-by: Himanshu Rai <swavyast@gmail.com>

--- src/com/ml/revision/reflection/Test2.java
+++ src/com/ml/revision/reflection/Test2.java
@@ -1,47 +1,67 @@
 package com.ml.revision.reflection;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
 
 public class Test2 {
-	
+
 	public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	private static final String[] COLOR_ARRAY = { ANSI_RESET, ANSI_RED, ANSI_GREEN, ANSI_YELLOW, ANSI_BLUE, ANSI_PURPLE,
+			ANSI_CYAN };
 
 	public static void main(String[] args) {
-		System.out.println(ANSI_RED+"Test2.main()");
+		System.out.println(COLOR_ARRAY[1] + "Test2.main()");
 		try {
 			Class clazz = Class.forName("com.ml.revision.reflection.Sample1");
-			Constructor<?> [] constructors  = clazz.getDeclaredConstructors();
-			for(Constructor c: constructors) {
-				c.setAccessible(true);
-				//c.setAccessible(constructors, true);
-				System.out.println(ANSI_GREEN+"\n\n++++++++++++++++++++++++++++++++++++++++++++\n\n");
-				System.out.println(ANSI_YELLOW+"getModifiers : "+c.getModifiers());
-				System.out.println(ANSI_BLUE+"getName : "+c.getName());
-				System.out.println(ANSI_PURPLE+"getParameterCount : "+c.getParameterCount());
-				System.out.println(ANSI_CYAN+"getGenericParameterTypes : "+c.getGenericParameterTypes());
-				System.out.println(ANSI_RED+"getParameters : "+c.getParameters());
-				System.out.println(ANSI_GREEN+"getTypeParameters : "+c.getTypeParameters());
-				System.out.println(ANSI_YELLOW+"isAccessible : "+c.isAccessible());
-				System.out.println(ANSI_BLUE+"isSynthetic : "+c.isSynthetic());
-				//System.out.println("newInstance : "+c.newInstance(args));
-				System.out.println(ANSI_PURPLE+"trySetAccessible : "+c.trySetAccessible());
-				System.out.println(ANSI_CYAN+"getClass : "+c.getClass());
-				System.out.println(ANSI_RED+"getDeclaringClass : "+c.getDeclaringClass());
-				System.out.println(ANSI_GREEN+"getExceptionTypes : "+c.getExceptionTypes());
-				System.out.println(ANSI_YELLOW+"getParameterTypes : "+c.getParameterTypes());
-				System.out.println(ANSI_BLUE+"getDeclaredAnnotations : "+c.getDeclaredAnnotations());
-				System.out.println(ANSI_PURPLE+"getDeclaredAnnotations: "+c.getGenericExceptionTypes());
-				System.out.println(ANSI_CYAN+"DECLARED : "+c.DECLARED);
-				System.out.println(ANSI_RED+"PUBLIC : "+c.PUBLIC);
-				System.out.println(ANSI_RESET+"\n\n*************************************************");
+
+			// fetching constructors details
+			Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+			for (Constructor<?> c : constructors) {
+				// c.setAccessible(true);
+				// c.setAccessible(constructors, true);
+				System.out.println(COLOR_ARRAY[2] + "\n\n++++++++++++++++++++++++++++++++++++++++++++\n\n");
+				System.out.println(COLOR_ARRAY[3] + "getModifiers : " + c.getModifiers());
+				System.out.println(COLOR_ARRAY[4] + "getName : " + c.getName());
+				System.out.println(COLOR_ARRAY[5] + "getParameterCount : " + c.getParameterCount());
+				Type paraTypes[] = c.getGenericParameterTypes();
+				int i = 6;
+				for (Type t : paraTypes) {
+					i++;
+					if (i > 6 || i < 0)
+						i = 0;
+					System.out.println(COLOR_ARRAY[i] + "getGenericParameterTypes -> getTypeName : " + t.getTypeName());
+
+				}
+				for (Parameter p : c.getParameters()) {
+					System.out.println(COLOR_ARRAY[1] + "getParameters -> getParameterizedType : " + p.getParameterizedType());
+					System.out.println(COLOR_ARRAY[2] + "getParameters -> getModifiers : " + p.getModifiers());
+					System.out.println(COLOR_ARRAY[3] + "getParameters -> getName : " + p.getName());
+					System.out.println(COLOR_ARRAY[4] + "getParameters -> getClass : " + p.getClass());
+					System.out.println(COLOR_ARRAY[5] + "getParameters -> getType : " + p.getType());
+				}
+
+				System.out.println(COLOR_ARRAY[0] + "getTypeParameters : " + c.getTypeParameters());
+				System.out.println(COLOR_ARRAY[1] + "isAccessible : " + c.isAccessible());
+				System.out.println(COLOR_ARRAY[2] + "isSynthetic : " + c.isSynthetic());
+				System.out.println(COLOR_ARRAY[3] + "trySetAccessible : " + c.trySetAccessible());
+				System.out.println(COLOR_ARRAY[4] + "getClass : " + c.getClass());
+				System.out.println(COLOR_ARRAY[5] + "getDeclaringClass : " + c.getDeclaringClass());
+				System.out.println(COLOR_ARRAY[6] + "getExceptionTypes : " + c.getExceptionTypes());
+				System.out.println(COLOR_ARRAY[0] + "getParameterTypes : " + c.getParameterTypes());
+				System.out.println(COLOR_ARRAY[1] + "getDeclaredAnnotations : " + c.getDeclaredAnnotations());
+				System.out.println(COLOR_ARRAY[2] + "getDeclaredAnnotations: " + c.getGenericExceptionTypes());
+				System.out.println(COLOR_ARRAY[3] + "DECLARED : " + c.DECLARED);
+				System.out.println(COLOR_ARRAY[4] + "PUBLIC : " + c.PUBLIC);
+				System.out.println(COLOR_ARRAY[5] + "\n\n*************************************************");
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
